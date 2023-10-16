19ddbb0d42a54a8ceea0bce20035883ac5549d3d
ri.gayfullin <ri.gayfullin@tensor.ru>
2023-10-16 11:17:51 +0300

    Initial Commit

---
+++ task6/task6App.swift
@@ -0,0 +1,17 @@
+//
+//  task6App.swift
+//  task6
+//
+//  Created by Руслан Гайфуллин on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct task6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
